e86c70bbf961ea9f81b50368707a4a742ffe0729
luukfroling <luukfroling@gmail.com>
2026-03-05 18:14:44 +1100

    prepend node

--- testplugin.mjs
+++ testplugin.mjs
@@ -2,15 +2,26 @@ const plugin = {
   name: 'Strong to emphasis',
   transforms: [
     {
-      name: 'transform-typography',
-      doc: 'An example transform that rewrites bold text as text with emphasis.',
-      stage: 'document',
-      plugin: (_, utils) => (node) => {
-        console.log('Transforming node:', node);
-        for (const child of node.children) {
-            console.log('Child node:', child);
+        name: 'transform-typography',
+        doc: 'An example transform that rewrites bold text as text with emphasis.',
+        stage: 'document',
+        plugin: (_, utils) => (node) => {
+            const nodeToAdd = {
+                    type: 'block',
+                    children: [
+                        {
+                        type: 'anywidget',
+                        esm: 'test_widget.mjs',
+                        model: [Object],
+                        css: undefined,
+                        class: undefined,
+                        id: 'Zu_TMHf4SUHXi9eGK7JCc'
+                        }
+                    ]
+            }
+            // add this note as the last child of the document
+            node.children.push(nodeToAdd);
         }
-      },
     },
   ],
 };
