7e7f56135890f98cf8a7551a5b90ad01c54cb4c5
luukfroling <luukfroling@gmail.com>
2026-03-05 18:16:19 +1100

    Update testplugin.mjs

--- testplugin.mjs
+++ testplugin.mjs
@@ -2,26 +2,20 @@ const plugin = {
   name: 'Strong to emphasis',
   transforms: [
     {
-        name: 'transform-typography',
-        doc: 'An example transform that rewrites bold text as text with emphasis.',
-        stage: 'document',
-        plugin: (_, utils) => (node) => {
-            const nodeToAdd = {
-                    type: 'block',
-                    children: [
-                        {
-                        type: 'anywidget',
-                        esm: 'test_widget.mjs',
-                        model: [Object],
-                        css: undefined,
-                        class: undefined,
-                        id: 'Zu_TMHf4SUHXi9eGK7JCc'
-                        }
-                    ]
-            }
-            // add this note as the last child of the document
-            node.children.push(nodeToAdd);
+      name: 'transform-typography',
+      doc: 'An example transform that rewrites bold text as text with emphasis.',
+      stage: 'document',
+      plugin: (_, utils) => (node) => {
+        console.log('Transforming node:', node);
+        for (const child of node.children) {
+            console.log('Child node:', child);
         }
+        // if node.children[0].children[0] exists, get its model object and log it
+        if (node.children[0] && node.children[0].children[0]) {
+            const model = utils.getModel(node.children[0].children[0]);
+            console.log('Model of first child:', model);
+        }
+      },
     },
   ],
 };
