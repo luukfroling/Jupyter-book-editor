72e232560aaa5ec606a3c0ba2b635869c32dcf4d
luukfroling <luukfroling@gmail.com>
2026-03-06 07:57:16 +1100

    Update testplugin.mjs

--- testplugin.mjs
+++ testplugin.mjs
@@ -1,19 +1,22 @@
 const plugin = {
-  name: 'Strong to emphasis',
+  name: 'Append anywidget to every page',
   transforms: [
     {
-      name: 'transform-typography',
-      doc: 'An example transform that rewrites bold text as text with emphasis.',
+      name: 'transform-add-anywidget',
+      doc: 'Adds an anywidget to the end of every page',
       stage: 'document',
       plugin: (_, utils) => (node) => {
-        console.log('Transforming node:', node);
-        for (const child of node.children) {
-            console.log('Child node:', child);
-        }
-        // if node.children[0].children[0] exists, get its model object and log it
-        if (node.children[0] && node.children[0].children[0]) {
-            const model = node.children[0].children[0].model;
-            console.log('Model of first child:', model);
+        const anyWidgetNode = {
+            type: 'block',
+            children: [
+                {
+                type: 'anywidget',
+                esm: 'test_widget.mjs',
+                model: {},
+                css: undefined,
+                class: undefined
+                }
+            ]
         }
       },
     },
